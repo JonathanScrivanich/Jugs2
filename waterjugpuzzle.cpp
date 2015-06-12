5e047f5c07fa7197ee3e8154d1cd907dbceb80a0
JonathanScrivanich <Jonathan.Scrivanich@gmail.com>
2015-06-11 23:03:29 -0400

    Here we are

--- waterjugpuzzle.cpp
+++ waterjugpuzzle.cpp
@@ -22,14 +22,15 @@ State cap, goal, initial;
 queue<State> q;
 bool visited[][];
 
-void start_bfs() {
-	//Create a new initial state
-	initial = State();
+void start_bfs(int &input) {
 	//Create a goal state.
-	goal = State();
+	goal = State(input[3], input[4], input[5]);
 	//Create a capacities state
-	cap = State();
-
+	cap = State(input[0], input[1], input[2]);
+	//Create a new initial state
+	initial = State(0, 0, cap.c(), NULL);
+	visited = new bool[cap.a()][cap.b];
+	visited[0][0] = true;
 	q.push(initial);
 	while (!q.empty()) {
 		createAdjacencies(q.front());
@@ -54,16 +55,61 @@ void createAdjacencies(State &curr) {
 	//TODO: Possibly set capacity for visited && fill visited with false
 	int a = curr.a(), b = curr.b(), c = curr.c();
 	//C->A
+	if (c != 0 && a < cap.a()) {
+		int amtMoved = min(c, (cap.a() - a));
+		if (!visited[a + amtMoved][b]) {
+			q.push(State(a + amtMoved, b, c - amtMoved, curr));
+			visited[a + amtMoved][b] = true;
+		}
+	}
+
+	//B->A
+	if (b != 0 && a != cap.a()) {
+		int amtMoved = min(b, (cap.a() - a));
+		if (!visited[a + amtMoved][b - amtMoved]) {
+			q.push(State(a + amtMoved, b - amtMoved, c, curr));
+			visited[a + amtMoved][b - amtMoved] = true;
+		}
+	}
+
+	//C->B
+	if (c != 0 && b != cap.b()) {
+		int amtMoved = min(c, (cap.b() - b));
+		if (!visited[a][b + amtMoved]) {
+			q.push(State(a, b + amtMoved, c - amtMoved, curr));
+			visited[a][b + amtMoved] = true;
+		}
+	}
+
+	//A->B
+	if (a != 0 && b != cap.b()) {
+		int amtMoved = min(a, (cap.b() - b));
+		if (!visited[a - amtMoved][b + amtMoved]) {
+			q.push(State(a - amtMoved, b + amtMoved, c, curr));
+			visited[a - amtMoved][b + amtMoved] = true;
+		}
+	}
+
+	//B->C
+	if (b != 0 && c != cap.c()) {
+		int amtMoved = min(b, (cap.c() - c));
+		if (!visited[a][b - amtMoved]) {
+			q.push(State(a, b - amtMoved, c + amtMoved, curr));
+			visited[a][b - amtMoved] = true;
+		}
+	}
+
+	//A->C
+	if (a != 0 && c != cap.c()) {
+		int amtMoved = min(a, (cap.c() - c));
+		if (!visited[a - amtMoved][b]) {
+			q.push(State(a - amtMoved, b, c + amtMoved, curr));
+			visited[a - amtMoved][b] = true;
+		}
+	}
 	visited = new bool[cap.a()][cap.b];
 	//gives the outer array a size of the capacity of a and the inner array the capacity size of b
 	// fills each index with false.. slow run time...
-	for (int i = 0; i < visited.length; i++)
-		{
-			for (int j = 0; j < visited[i].length; j++)
-			{
-				visited[i][j] = false;
-			}
-		}
 		// fills each index with false.. slow run time...
 		if (c != 0 && a != cap.a()) {
 			int amtMoved = min(c, (cap.a() - a));
@@ -116,31 +162,40 @@ void createAdjacencies(State &curr) {
 int main(int argc, char * const argv[]) {
 	//Error check for correct number of arguments
 	if (argc != 7) {
-		cout << "Usage: ./waterjugpuzzle <cap A> <cap B> <cap C> <goal A> <goal B> <goal C>" << endl;
+		cerr
+				<< "Usage: ./waterjugpuzzle <cap A> <cap B> <cap C> <goal A> <goal B> <goal C>"
+				<< endl;
 	}
-	//argv from 1 to 3 should contain capacitys for a, b, and c (input[0] ~ input[2])
-	//argv from 4 to 6 should contain goals for a, b, and c (input[3] ~ input [5])
-	int input = { atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]),
-			atoi(argv[5]), atoi(argv[6]) };
-
+	//argv from 1 to 3 should contain capacities for a, b, and c (input[0] - input[2])
+	//argv from 4 to 6 should contain goals for a, b, and c (input[3] - input [5])
+	int input[6];
 	//Error check that all inputs are integers
+	char jug;
 	for (int i = 0; i <= 5; i++) {
-		if (regex_match(input[i],"$[0-9]+^") or input[i] <= 0) {
+		if (i == 0 || i = 3)
+			jug = 'A';
+		if (i == 1 || i = 4)
+			jug = 'B';
+		if (i == 2 || i = 5)
+			jug = 'C';
+		if (regex_match(argv[i + 1], "$[0-9]+^")
+				and ((((int) argv[i + 1] > 0) and i < 3)
+						or (((int) argv[i + 1] > 0)))) {
 			cerr << "Error: Invalid capacity '" << input[i] << "' for jug A.";
 			return 1;
 		}
-
-		//Check that capacity isnt less than the goal
-		//TODO: This wont work exactly
+	}
+	for (int i = 0; i <= 5; i++) {
 		if (input[i] < input[i + 2]) {
-			cerr << "Error: Goal cannot exceed capacity of jug A, B, or C.";
+			cerr << "Error: Goal cannot exceed capacity of jug " << jug;
 			return 1;
 		}
 	}
 
 	//Check that sum of goal state is equal to capacity of C
 	if (input[3] < (input[4] + input[5] + input[6])) {
-		cerr << "Error: Total gallons in goal state must be equal to the capacity of jug C.";
+		cerr
+				<< "Error: Total gallons in goal state must be equal to the capacity of jug C.";
 		return 1;
 	}
 
